5e0fbca8dbcca37ee28a62f3733176dd0a29b38a
maor63 <maor309@gmail.com>
2017-11-27 16:49:00 +0200

    Improved read file speed

--- ReadFileTest.py
+++ ReadFileTest.py
@@ -7,20 +7,42 @@ class ReadFileTestCase(unittest.TestCase):
     def setUp(self):
         self.read_file = ReadFile()
 
-    def test_read_file(self):
-        docs = self.read_file.read_docs("./test_data/FB396011/FB396011")
+    def test_read_FB_file(self):
+        docs = self.read_file.read_docs_from_FB_file("./test_data/FB396011/FB396011")
         self.assertEqual(265, len(docs))
-        self.assertIsNotNone(docs[2].id)
-        self.assertIsNotNone(docs[2].text)
+        self.assertEqual("FBIS3-2550", docs[3].id)
 
-    def test_read_files_from_path(self):
-        docs = self.read_file.read_files("./test_data/")
-        self.assertEqual(347 + 176, len(docs))
+    def test_read_FT_file(self):
+        docs = self.read_file.read_docs_from_FT_file("./test_data/FT942_40/FT942_40")
+        self.assertEqual(289, len(docs))
+        self.assertEqual("FT942-13574", docs[3].id)
+        text = "Nearly 40 teenage rugby players were hurt when the bus taking them from" + "\n" + \
+               "Stirling to a tournament at the border town of Hawick was in collision with" + "\n" + \
+               "a tanker at Middleton Moor, near Edinburgh."
+        self.assertEqual(text, docs[3].text)
 
-    def test_doc_with_no_text(self):
-        docs = self.read_file.read_docs("./test_data/FB396070/FB396070")
+    def test_read_LA_file(self):
+        docs = self.read_file.read_docs_from_LA_file("./test_data/LA010790/LA010790")
+        self.assertEqual(226, len(docs))
+        self.assertEqual("LA010790-0008", docs[7].id)
+        text = '''"What Are We Doing to Our Children?," KTLA's program on the foster care and 
+juvenile justice system, presented an honest, realistic and disturbing 
+portrayal of what happens when parents fail and children are left at the mercy 
+of an overwhelmed and ineffective governmental agency. What is more alarming is 
+that, with the increase in drug use, domestic violence, infant abandonments, 
+etc., we are seeing more and more children swept into a whirlpool of 
+instability and danger. 
+
+Gary Traxler, North Hollywood 
+'''
+        self.assertEqual(text, docs[7].text)
+
+
+    def test_doc_with_missing_text_tag(self):
+        docs = self.read_file.read_docs_from_FB_file("./test_data/FB396070/FB396070")
         print(len(docs))
         self.assertEqual(176, len(docs))
+
 
 
 if __name__ == '__main__':
